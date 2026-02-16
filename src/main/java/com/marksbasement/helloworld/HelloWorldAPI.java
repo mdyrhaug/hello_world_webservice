b8d9648da06ecf7b0c09904d744487cca80f27eb
Mark Dyrhaug <mark@marksbasement.com>
2026-02-16 15:08:33 -0600

    simple development

--- src/main/java/com/marksbasement/helloworld/HelloWorldAPI.java
+++ src/main/java/com/marksbasement/helloworld/HelloWorldAPI.java
@@ -4,15 +4,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author Mark Dyrhaug
+ * @ since 02/16/2026
+ *
+ * This class is a simple endpoint class.
+ */
 @RestController
-public class HelloWorldAPI {
+public class  HelloWorldAPI {
 
-    private static final Logger log = LoggerFactory.getLogger(HelloWorldAPI.class);
+    /**
+     * General logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldAPI.class);
 
+    /**
+     * This endpoint will return the string 'Hello World.
+     * @return String Hello World
+     */
     @GetMapping("/helloWorld")
     public String helloWorld() {
-        if (log.isInfoEnabled()) {
-            log.info("/helloWorld endpoint has been called");
+        if (LOG.isInfoEnabled()) {
+            LOG.info("/helloWorld endpoint has been called");
         }
         return "Hello World!";
     }
---
+++ src/main/java/com/marksbasement/helloworld/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes necessary to perform a 'hello world' endpoint.
+ */
+package com.marksbasement.helloworld;
+
