5b42df50ebd01dc76bb9ecb947f8f1dd440dd878
markd <mark@marksbasement.com>
2026-02-17 18:52:41 -0600

    fixed dependencies

--- src/main/java/com/marksbasement/helloworld/HelloWorldApplication.java
+++ src/main/java/com/marksbasement/helloworld/HelloWorldApplication.java
@@ -2,13 +2,18 @@ package com.marksbasement.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HelloWorldApplication.class);
+	}
 }
